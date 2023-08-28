59abdf4f15e1e79d547f1d8bcf55f360ef5b2a89
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-08-28 16:43:06 +0100

    installed mongoose and use to it to connect to a mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require("./routes/workouts")
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -10,9 +11,14 @@ app.use((req, res, next) =>{
     console.log(req.path, req.method)
     next()
 })
-
+//connect to mongodb
+mongoose.connect(process.env.URI)
+.then(() =>{
+        //listening to request
+        app.listen(process.env.PORT, () =>{
+            console.log("listening on port 8080 & connected to mongodb")
+})
+})
+.catch(err => console.log(err))
 //routes
-app.use("/api/workouts", routes)
-
-//listening to request
-app.listen(process.env.PORT)
+app.use("/api/workouts", routes);
